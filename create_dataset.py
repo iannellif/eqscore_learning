eb06483bf58470414f5b4be760ff5e4f61d43296
Flavio Iannelli <iannelli.flavio@gmail.com>
2022-04-11 16:41:33 +0200

    Add files via upload

--- create_dataset.py
+++ create_dataset.py
@@ -1,46 +1,51 @@
+
+
 import pandas as pd
 import numpy as np
 
 
 
+
 #number of examples
 m=100
-#number of features (semantics + flavours)
+#number of features 
 n=4
 
 
 
 np.random.seed(0)
 xsem = 1- 0.5 * np.random.randint(0,3, size=(1, m))
+xsem
+
 
 np.random.seed(0)
-xfla = 1- 0.5 * np.random.randint(2, size=(n-1, m))
+xfla = np.random.randint(2, size=(n-1, m))
+xfla.shape
 
 x = np.concatenate((xsem,xfla), axis=0)
 
-xfull = np.concatenate((np.asarray([np.ones(m)]),x), axis=0)
-
-np.random.seed(0)
-y = (np.random.randint(50,100,m)/100).round(1)
+# xfull = np.concatenate((np.asarray([np.ones(m)]),x), axis=0)
+xfull = x
 
 
 
 
 # # Linear Model
 
+
 np.random.seed(0)
-weights = np.random.randint(1,10,n+1)
+weights = np.random.randint(1,10,n)
 wnorm = weights/sum(weights)
 
 ylin = np.dot(xfull.T,wnorm).round(1)
 
 
-
 # # Datasets
+
+
 
 data_label = np.concatenate((x, np.asarray([ ylin ])), axis=0)
 data_unlab = x
-
 
 colnames_label = ['semantics', 'frequency', 'vulgarity', 'universality', 'score']
 colnames_unlab = ['semantics', 'frequency', 'vulgarity', 'universality']
@@ -53,9 +58,12 @@ dataset_label.to_csv(path+filename,index=False)
 
 dataset_unlab = pd.DataFrame(data_unlab.T,columns=colnames_unlab,dtype=float)
 
+
 path = 'data/'
 filename = 'data_eqs_random_unlab.csv'
 dataset_unlab.to_csv(path+filename,index=False)
+
+
 
 
 
